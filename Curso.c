b639fe2e63e5f4dedac488aedb9686f7cb430237
diogopelinson <diogopelinsonduartemoraes@gmail.com>
2025-03-18 22:49:34 -0300

    Adicionando mais funcoes

--- Curso.c
+++ Curso.c
@@ -11,25 +11,30 @@ int main() {
 
     int chute;
 
-   printf("Qual e o seu chute?: ");
-   scanf("%d", &chute);
-   printf("Seu chute foi %d\n", chute);
+    // Loop do codigo
+    for(int i = 1; i <= 3; i++) {
+
+    printf("Tentativa %d de 3\n", i );
+    printf("Qual e o seu chute?: ");
+
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
 
     int acertou = (chute == numerosecreto);
 
-      if(acertou) {
-    printf("Parabens!! Voce acertou!!\n");
-    printf("Jogue de novo, voce e um bom jogador!");
-    }
+        if(acertou) {
+        printf("Parabens!! Voce acertou!!\n");
+        printf("Jogue de novo, voce e um bom jogador!");
+        }
 
-    else {
-
-        int maior = chute > numerosecreto;
-    if(maior) { 
-        printf("Seu chute foi maior que o numero secreto!\n");
-    }
         else {
-            printf("Seu chute foi menor que o numero secreto!\n");
+            int maior = chute > numerosecreto;
+        if(maior) { 
+            printf("Seu chute foi maior que o numero secreto!\n");
+        } else {
+                printf("Seu chute foi menor que o numero secreto!\n");
+            }
         }
     }
+    printf("Fim de jogo!\n");
 }
